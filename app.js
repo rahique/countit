4dcfed53c901f91936f76b66eb0ffdf559fbc197
Rahique <rahiqueosman@gmail.com>
2024-01-27 01:51:15 +0600

    total added

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-let count = document.querySelector(".count");
-let write = document.querySelector(".previous");
+let count = document.querySelector('.count');
+let write = document.querySelector('.previous');
+let total = document.querySelector('.total');
 
 let countEl = 0;
+let totalCount = 0;
 
 function increment() {
   countEl++;
@@ -16,18 +18,27 @@ function decrement() {
   }
 }
 function save() {
-  write.innerText += " " + countEl + " - ";
+  write.innerText += ' ' + countEl + ' • ';
+  totalCount += countEl;
+  total.innerText = totalCount;
   let entryStore = write.innerText.substr(16);
-  localStorage.setItem("entry", entryStore);
+  localStorage.setItem('entry', entryStore);
+  localStorage.setItem('total', totalCount);
 }
 function erase() {
-  write.innerText = "Previous Entry : ";
+  write.innerText = 'Previous Entry : ';
+  total.innerText = ' ';
+  totalCount = 0;
   localStorage.clear();
 }
 function callStorage() {
-  let storage = localStorage.getItem("entry");
+  let storage = localStorage.getItem('entry');
+  let totalStorage = localStorage.getItem('total');
   if (storage != null) {
-    write.innerText += " " + storage;
+    write.innerText += ' ' + storage;
+    total.innerText += ' ' + totalStorage;
+    totalCount = parseInt(totalStorage);
+    
   } else {
     return;
   }
